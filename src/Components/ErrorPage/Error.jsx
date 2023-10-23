6bc2b7b1d63bce722b0bcbcc66a57c33a6c2fa8e
fardin36 <thunder.fardin@gmail.com>
2023-10-23 23:23:58 +0600

    404 not found page

--- src/Components/ErrorPage/Error.jsx
+++ src/Components/ErrorPage/Error.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
         <div>
-            Error 404 not found
+            <main className="h-screen w-full flex flex-col justify-center items-center bg-black">
+                <h1 className="text-9xl font-extrabold tracking-widest text-white">404</h1>
+                <div className="px-2 text-sm font-medium rounded rotate-12 absolute bg-black text-white border border-white">
+                    Page Not Found
+                </div>
+                <Link to="/" className="border px-5 py-3 text-white rounded-lg font-medium">Go Home</Link>
+            </main>
         </div>
     );
 };
