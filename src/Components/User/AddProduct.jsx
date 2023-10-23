51fed1058c4b3c5e016f82782384b290fda83367
fardin36 <thunder.fardin@gmail.com>
2023-10-24 00:06:59 +0600

    add new product page

--- src/Components/User/AddProduct.jsx
+++ src/Components/User/AddProduct.jsx
@@ -1,9 +1,63 @@
 
 const AddProduct = () => {
+
+    const handleAddProduct = e => {
+        e.preventDefault();
+        const form = e.target;
+
+        const image = form.image.value;
+        const name = form.name.value;
+        const brand = form.brand.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const rating = form.rating.value;
+
+        const newProduct = {
+            image, name, brand, type, price, description, rating,
+        }
+        console.log(newProduct);
+    }
+
     return (
-        <div>
-            Add Products
+        <div className="min-h-screen flex justify-center items-center w-full px-20">
+            <form className="w-full" onSubmit={handleAddProduct}>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="image" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                    <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Image URL</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                    <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product name</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="brand" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                    <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Brand name</label>
+                </div>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input type="text" name="type" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                        <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Category</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input type="text" name="price" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                        <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Price</label>
+                    </div>
+                </div>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input type="text" name="description" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                        <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Short description</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input type="text" name="rating" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#ffb300] peer" placeholder=" " required />
+                        <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#ffb300] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Rating</label>
+                    </div>
+                </div>
+                <button type="submit" className="text-white bg-[#ffb300] hover:bg-[#ffb300d5] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Submit</button>
+            </form>
         </div>
+
     );
 };
 
