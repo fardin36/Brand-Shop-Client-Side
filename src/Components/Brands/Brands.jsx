fd43fa0923288133b5c8aea5122d594b826c500a
fardin36 <thunder.fardin@gmail.com>
2023-10-23 22:49:45 +0600

    added brands section

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,8 +1,36 @@
 
+
 const Brands = () => {
     return (
-        <div>
-            Brand Names
+        <div className="min-h-screen flex flex-wrap justify-center items-center text-4xl">
+
+
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/hXpNNk2/apple-logo.png" alt="apple logo" />
+                <h5>APPLE</h5>
+            </div>
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/xDtjNsK/samsung-logo.png" alt="apple logo" />
+                <h5>SAMSUNG</h5>
+            </div>
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/5jZqSF6/sony-logo.png" alt="apple logo" />
+                <h5>SONY</h5>
+            </div>
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/48ny5Sq/LG-logo.png" alt="apple logo" />
+                <h5>LG</h5>
+            </div>
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/kmB0bvv/Microsoft-logo.png" alt="apple logo" />
+                <h5>MICROSOFT</h5>
+            </div>
+            <div className="w-full max-w-sm rounded-lg flex flex-col justify-center items-center">
+                <img className="rounded-lg h-48" src="https://i.ibb.co/zxQpGDv/Canon-logo.png" alt="apple logo" />
+                <h5>CANON</h5>
+            </div>
+
+
         </div>
     );
 };
